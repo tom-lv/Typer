23b638bc79321eaf2ccc78a9788330df8de9b695
tom-lv <tomlv.dev@gmail.com>
2022-06-14 01:16:23 +0100

    Display active word-count

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-let wordCount = 50;
+let wordCount; // num. of words typing test words
 
 // fetch json data
 function getText() {
@@ -13,16 +13,22 @@ function getText() {
 
 // displayed fetched text
 function displayText(data) {
-    let typingArea = document.querySelector(".typing-area p")
-    typingArea.innerHTML = ""; //make area is sure empty
+    const typingArea = document.querySelector('.typing-area p');
+    typingArea.innerHTML = ""; //make sure area is empty
     for (let i = 0; i < wordCount; i++) {
         typingArea.innerHTML += data.words[i] + " ";
     }
 }
 
 function setWordCount(value) {
+    // set wordCount as argument passed to the function
     wordCount = value;
-    getText();
+    // set color:black all button elements under .word-count-selector
+    document.querySelectorAll('.word-count-selector > button').forEach(e => (e.style.color = 'black'));
+    // set active button color:purple
+    document.querySelector(`#wc-${value}`).style.color = '#9256ED';
+    getText(); // update text
 }
 
-getText();
+setWordCount(50); // default value
+getText(); // display text
