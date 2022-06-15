b5aa5f13958999438ffde9564e2a249ce608b615
tom-lv <tomlv.dev@gmail.com>
2022-06-15 18:20:18 +0100

    Add char highlighting & change char state
    Each character now has three different states.Namely, active, correct and incorrect.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
 const typingArea = document.querySelector('.typing-area');
 
-let wordNum; // num. of words typing test words
+let wordCount; // num. of words typing test words
 let wordLanguage; // typing test language
 let randomWordList = []; // store random list of words
 let iteration; // keep track of loop iterations
+let charIndex = 0; // keep track of chars
 
 // fetch json data
 function getText() {
@@ -15,7 +16,7 @@ function getText() {
         .catch(err => console.error(err));
 }
 
-// displayed fetched text
+// display fetched text
 function displayText(data) {
     iteration = 0; // reset iteration
     typingArea.innerHTML = ""; // make sure typing area is empty
@@ -28,30 +29,51 @@ function displayText(data) {
         });
         iteration++;
         // include space after each word expect last
-        if (iteration != wordNum) {
+        if (iteration != wordCount) {
             let span = document.createElement('span');
             span.innerHTML = ' ';
             typingArea.appendChild(span);
         }
     });
+    // set first span char as 'active'
+    typingArea.querySelectorAll('.typing-area > span')[0].classList.add('active');
 }
 
 // randomise fetched words
 function randomiseWords(data) {
     randomWordList = []; // make sure array is empty
-    for (let i = 0; i < wordNum; i++) {
+    for (let i = 0; i < wordCount; i++) {
         let randomIndex = Math.floor(Math.random() * data.words.length);
         randomWordList.push(data.words[randomIndex]);
     }
     displayText(randomWordList);
 }
 
-function setWordNum(value) {
-    wordNum = value;
+function startApplication() {
+    document.addEventListener('keypress', e => {
+        console.log(e.key);
+        console.log(typingArea.querySelector('.active').textContent);
+        let activeChar = typingArea.querySelector('.active').textContent;
+        if (e.key === activeChar) {
+            document.querySelector('.active').classList.add('correct');
+            document.querySelector('.active').classList.remove('active');
+            charIndex++;
+            console.log(charIndex);
+        } else {
+            document.querySelector('.active').classList.add('incorrect');
+            document.querySelector('.active').classList.remove('active');
+            charIndex++;
+        }
+        typingArea.querySelectorAll('.typing-area > span')[charIndex].classList.add('active');
+    });
+}
+
+function setWordCount(value) {
+    wordCount = value;
     // set color:black all button elements under .word-num-selector
     document.querySelectorAll('.word-num-selector > button').forEach(e => (e.style.color = 'black'));
     // set active button color:purple
-    document.querySelector(`#wn-${value}`).style.color = '#9256ED';
+    document.querySelector(`#wc-${value}`).style.color = '#9256ED';
     getText(); // update text
 }
 
@@ -65,9 +87,11 @@ function setLanguage(language) {
 }
 
 function resetApplication() {
-    setWordNum(wordNum);
+    setWordCount(wordCount);
+    charIndex = 0;
 }
 
 // Initial state
 setLanguage("en"); // default language
-setWordNum(100); // default value
+setWordCount(50); // default value
+startApplication();
